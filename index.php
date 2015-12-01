df258a3e2039bc144f28fe5e28827e40e9715976
LopezRueda <marco_daniel_03@hotmail.com.ar>
2015-12-01 02:11:24 -0300

    AlquilerPrueba

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Datos de Alumno</title>
+    </head>
+    <body>
+        <?php
+          echo "Alumno : Rueda Fernando Bartolomè";
+          echo "LU : 2732";
+          echo "Correo : marco_daniel_03@hotmail.com.ar";
+          echo "Celular : 3884614899";
+        ?>
+    </body>
+</html>
